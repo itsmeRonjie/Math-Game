0f6e5068909a1e34006d7e4534dc7e4c3f490d40
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-09 14:56:48 +0800

    - added CRUD in ViewModel

--- Math-Game/ViewModel/HighScoreViewModel.swift
+++ Math-Game/ViewModel/HighScoreViewModel.swift
@@ -31,14 +31,48 @@ class HighScoreViewModel {
     }
     
     func fetchHighScores() {
-        let request = NSFetchRequest<HighScoreEntity>(
-            entityName: "HighScoreEntity"
+        let request = NSFetchRequest<HighScoreEntity>(entityName: "HighScoreEntity")
+        let sortDescriptor = NSSortDescriptor(
+            keyPath: \HighScoreEntity.score,
+            ascending: false
         )
+        
+        request.sortDescriptors = [sortDescriptor]
         
         do {
             highScores = try container.viewContext.fetch(request)
         } catch let error as NSError {
             print("Error fetching high scores: \(error.localizedDescription)")
         }
+    }
+    
+    func saveHighScores() {
+        do {
+            try container.viewContext.save()
+            fetchHighScores()
+        } catch let error as NSError {
+            print("Error saving high scores: \(error.localizedDescription)")
+        }
+    }
+    
+    func addNewHighScore(name: String, score: Int64) {
+        let entity = HighScoreEntity(context: container.viewContext)
+        entity.name = name
+        entity.score = score
+        
+        saveHighScores()
+    }
+    
+    func updateHighScore(entity: HighScoreEntity, name: String) {
+        let entity = HighScoreEntity(context: container.viewContext)
+        entity.name = name
+        
+        saveHighScores()
+    }
+    
+    func deleteHighScore(entity: HighScoreEntity) {
+        container.viewContext.delete(entity)
+        
+        saveHighScores()
     }
 }
